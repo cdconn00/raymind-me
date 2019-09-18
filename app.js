a21874471a8e17640c12047a3e119cbb72c69de5
Cole Connelly <30357632+cdconn00@users.noreply.github.com>
2019-09-17 19:59:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
 	// stores current user
 	res.locals.currentUser = req.user;
 
